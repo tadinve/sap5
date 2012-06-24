0993275dcb5c684b0dee186a3233c44d5a13cfad
Venkatesh Tadinada <venkat@solivar.com>
2012-06-23 20:55:17 -0700

    finished chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Sap5::Application.routes.draw do
+
+  get "users/new"
+
+   root to: "static_pages#home"
+
+  match "/help", to: "static_pages#help"
+  match "/signup", to: "users#new"
+  match "/about", to: "static_pages#about"
+  match "/contact", to: "static_pages#contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
